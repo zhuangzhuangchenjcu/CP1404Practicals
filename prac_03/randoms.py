14f2db2c956c960661b0c9779c9a4803ce2c6604
zhuangzhuangchenjcu <zhuangzhuang.chen@my.jcu.edu.au>
2024-09-28 23:02:22 +0800

    Add files via upload

---
+++ prac_03/randoms.py
@@ -0,0 +1,10 @@
+import random
+print(random.randint(5, 20))
+#  5  19
+print(random.randrange(3, 10, 2))
+#  3  9
+print(random.uniform(2.5, 5.5))
+#  2.5  5.49999999
+import random
+x = random.randint(1,101)
+print(x)
