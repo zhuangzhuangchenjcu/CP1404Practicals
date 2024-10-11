2b1a35d1a0724191c9b61bb92f62ec70bea9e92d
zhuangzhuangchenjcu <zhuangzhuang.chen@my.jcu.edu.au>
2024-10-11 19:17:16 +0800

    Add files via upload

---
+++ prac_04/lists_warmup.py
@@ -0,0 +1,9 @@
+numbers = [3, 1, 4, 1, 5, 9, 2]
+# 1.Change the first element of numbers to "ten".
+numbers[0] = "ten"
+# 2.Change the last element of numbers to 1
+numbers[-1] = 1
+# 3.Print all the elements from numbers except the first two
+print(numbers[2:])
+# 4.Print whether 9 is an element of numbers
+print(9 in numbers)
---
+++ prac_04/subject_reader.py
@@ -0,0 +1,38 @@
+"""
+CP1404/CP5632 Practical
+Data file -> lists program
+"""
+
+FILENAME = "subject_data.txt"
+
+
+def main():
+    data = get_data()
+    print(data)
+    display_subjects(data)
+
+
+def get_data():
+    """Read data from file formatted like: subject,lecturer,number of students."""
+    data = []
+    input_file = open(FILENAME)
+    for line in input_file:
+        print(line)  # See what a line looks like
+        print(repr(line))  # See what a line really looks like
+        line = line.strip()  # Remove the \n
+        parts = line.split(',')  # Separate the data into its parts
+        print(parts)  # See what the parts look like (notice the integer is a string)
+        parts[2] = int(parts[2])  # Make the number an integer (ignore PyCharm's warning)
+        print(parts)  # See if that worked
+        data.append(parts)
+    input_file.close()
+    return data
+
+
+def display_subjects(data):
+    for subject_data in data:
+        print(f"{subject_data[0]} is taught by {data[1]:12} and has {data[2]:3} students")
+
+
+main()
+
