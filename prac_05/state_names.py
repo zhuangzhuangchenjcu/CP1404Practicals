c24bed22c3942583ed5db3307a723def048ec39d
zhuangzhuangchenjcu <zhuangzhuang.chen@my.jcu.edu.au>
2025-06-22 15:37:31 +0800

    state_names.py

--- prac_05/state_names.py
+++ prac_05/state_names.py
@@ -9,7 +9,7 @@ CODE_TO_NAME = {"QLD": "Queensland", "NSW": "New South Wales",
                 "ACT": "Australian Capital Territory", "VIC": "Victoria",
                 "TAS": "Tasmania"}
 print(CODE_TO_NAME)
- 
+  
 state_code = input("Enter short state: ").upper()
 while state_code != "":
     try:
