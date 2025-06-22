411f006be96a9ff9d167d33f3a085bf59810ebf3
zhuangzhuangchenjcu <zhuangzhuang.chen@my.jcu.edu.au>
2025-06-22 15:39:19 +0800

    word_occurrences.py

--- prac_05/word_occurrences.py
+++ prac_05/word_occurrences.py
@@ -8,7 +8,7 @@ for word in words:
 
 words = list(word_to_count.keys())
 words.sort()
-
+ 
 max_length = max((len(word) for word in words))
 for word in words:
     print("{word:{max_length}} : {word_to_count[word]}")
